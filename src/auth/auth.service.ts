d99eb47269001b675c0a8ddb8628d15f4e26fbc0
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-14 02:09:36 +0300

    create register and login methods with jwt auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { LoginDto, RegistrationDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-  register() {}
+  async register(dto: RegistrationDto) {
+    const { email, password, username, avatar } = dto;
 
-  login() {}
+    const hashedPassword = await argon.hash(password);
+
+    try {
+      const newUser = await this.prisma.user.create({
+        data: {
+          username,
+          email,
+          hashedPassword,
+          avatar,
+        },
+      });
+
+      return this.signToken(newUser.id, newUser.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002')
+          throw new ForbiddenException('Credentials must be unique');
+      }
+    }
+  }
+
+  async login(dto: LoginDto) {
+    const { email, password } = dto;
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { email },
+      });
+
+      if (!user) return new NotFoundException('User not found');
+
+      const passwordMatch = await argon.verify(password, user.hashedPassword);
+
+      if (!passwordMatch) return new ForbiddenException('Incorrect Password');
+
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002')
+          throw new ForbiddenException('Credentials must be unique');
+      }
+    }
+  }
+
+  async signToken(userId: number, email: string) {
+    const token = await this.jwt.signAsync(
+      { userId, email },
+      {
+        expiresIn: '1h',
+        secret: this.config.get('JWT_SECRET'),
+      },
+    );
+
+    return { token };
+  }
 }
