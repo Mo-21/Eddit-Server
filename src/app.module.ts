96cd4ebcf58720d7846a70f4ec5200d902d56fe1
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-13 23:51:56 +0300

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { PostModule } from './post/post.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PostModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PostModule,
+    PrismaModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
