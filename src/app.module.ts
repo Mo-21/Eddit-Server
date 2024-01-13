fd0365e77ec323ebf6a737317fd6d20aa4af3da8
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-13 22:55:16 +0300

    generate post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PostModule } from './post/post.module';
+import { PostNoSpecController } from './post-no-spec/post-no-spec.controller';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [PostModule],
+  controllers: [PostNoSpecController],
   providers: [],
 })
 export class AppModule {}
