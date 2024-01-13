535afb96f5c7f677fdb72796021f1e4e9ce3ae79
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-14 01:36:02 +0300

    add register and login methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginDto, RegistrationDto } from './dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
   @Post('register')
-  register() {}
+  register(@Body() dto: RegistrationDto) {
+    return this.authService.register(dto);
+  }
 
   @Post('login')
-  login() {}
+  login(dto: LoginDto) {
+    return this.authService.login(dto);
+  }
 }
