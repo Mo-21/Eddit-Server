3a8e80732634af284ff73af58a44c794a3891ae7
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-13 23:45:32 +0300

    create post request to create one post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { PostService } from './post.service';
+import { PostDto } from './dto/post.dto';
 
 @Controller('post')
 export class PostController {
@@ -8,5 +9,11 @@ export class PostController {
   @Get('all')
   getAllPosts() {
     return this.postService.getAllPosts();
+  }
+
+  @HttpCode(201)
+  @Post('create')
+  createPost(@Body() dto: PostDto) {
+    return this.postService.createPost(dto);
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,11 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PostDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class PostService {
   constructor(private prisma: PrismaService) {}
+
   async getAllPosts() {
-    const posts = await this.prisma.post.findMany();
-    return posts;
+    try {
+      const posts = await this.prisma.post.findMany();
+      return posts;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async createPost(dto: PostDto) {
+    try {
+      const newPost = await this.prisma.post.create({
+        data: {
+          content: dto.content,
+          userId: dto.userId,
+        },
+      });
+
+      if (!newPost)
+        return new InternalServerErrorException('Something went wrong');
+
+      return newPost;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new ForbiddenException('Credentials must be provided');
+      }
+    }
   }
 }
