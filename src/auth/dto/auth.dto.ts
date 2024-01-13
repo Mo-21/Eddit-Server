52d25179caa7419ab44dfce0b69b6345dcf043a3
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-14 02:09:52 +0300

    add error message to each rule

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,29 +1,36 @@
-import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  Length,
+  IsEmail,
+  IsOptional,
+} from 'class-validator';
 
 export class RegistrationDto {
-  @IsString()
-  @IsNotEmpty()
-  @Length(1, 255)
+  @IsString({ message: 'Username must be a string' })
+  @IsNotEmpty({ message: 'Username is required' })
+  @Length(1, 255, { message: 'Username must be between 1 and 255 characters' })
   username: string;
 
-  @IsEmail()
-  @IsNotEmpty()
+  @IsEmail({}, { message: 'Invalid email format' })
+  @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'Password must be a string' })
+  @IsNotEmpty({ message: 'Password is required' })
   password: string;
 
-  @IsString()
+  @IsString({ message: 'Avatar must be a string' })
+  @IsOptional()
   avatar: string;
 }
 
 export class LoginDto {
-  @IsEmail()
-  @IsNotEmpty()
+  @IsEmail({}, { message: 'Invalid email format' })
+  @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'Password must be a string' })
+  @IsNotEmpty({ message: 'Password is required' })
   password: string;
 }
