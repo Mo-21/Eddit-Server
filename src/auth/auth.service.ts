a6c19fb165e74a258ba2acd0ad04617f371f16df
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-16 04:13:38 +0300

    return new user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -29,8 +29,7 @@ export class AuthService {
           avatar,
         },
       });
-
-      return this.signToken(newUser.id, newUser.email);
+      return newUser;
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002')
